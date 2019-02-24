b1984eba8211950afe11d6f54f9d0fb147aea044
Bahmet Lesia <bahmet@MacBook-Pro-8.local>
2019-02-24 10:58:03 +0200

    start new port when current is busy

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -4,33 +4,43 @@ import chalk from 'chalk';
 import DevServer from 'webpack-dev-server';
 import hot from 'webpack-hot-middleware';
 import openBrowser from 'react-dev-utils/openBrowser';
+import { choosePort } from 'react-dev-utils/WebpackDevServerUtils';
 
 import {HOST, PORT} from './constants';
 
 const compiler = webpack(getConfig());
 
-const server = new DevServer(compiler, {
-    host: HOST,
-    port: PORT,
-    historyApiFallback: true,
-    overlay: true,
-    quiet: true,
-    clientLogLevel: 'none',
-    noInfo: true,
-    after: (app) => {
-        app.use(
-            hot(compiler, {
-                log: false,
-            }),
+(async () => {
+    const port = await choosePort(HOST, PORT);
+
+    const server = new DevServer(compiler, {
+        host: HOST,
+        port,
+        historyApiFallback: true,
+        overlay: true,
+        quiet: true,
+        clientLogLevel: 'none',
+        noInfo: true,
+        after: (app) => {
+            app.use(
+                hot(compiler, {
+                    log: false,
+                }),
+            );
+        }
+    });
+
+    server.listen(port, HOST, () => {
+        const url = `http://${HOST}:${port}`;
+        console.log(
+            `${chalk.greenBright('Server listening on')} ${chalk.blueBright(`http://${HOST}:${port}`)}`
         );
-    }
-});
 
-server.listen(PORT, HOST, () => {
-    const url = `http://${HOST}:${PORT}`;
-    console.log(
-        `${chalk.greenBright('Server listening on')} ${chalk.blueBright(`http://${HOST}:${PORT}`)}`
-    );
-
-    openBrowser(url);
-});
+        openBrowser(url);
+    }).catch(err => {
+        if (err && err.message) {
+            console.log(err.message);
+        }
+        process.exit(1);
+    });
+})();
